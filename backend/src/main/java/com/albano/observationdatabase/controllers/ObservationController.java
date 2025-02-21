ad68971d150feb2b62c48d20d06e36a92147c87e
Arvid Forsberg <aforsberg97@gmail.com>
2025-02-21 14:15:38 +0100

    Added simple HTML website for entering new observations.

--- backend/src/main/java/com/albano/observationdatabase/controllers/ObservationController.java
+++ backend/src/main/java/com/albano/observationdatabase/controllers/ObservationController.java
@@ -3,9 +3,11 @@ package com.albano.observationdatabase.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,12 +22,14 @@ public class ObservationController {
     private ObservationRepository observationRepository;
 
     @PostMapping
-    public Observation addObservation(@RequestBody Observation observation) {
-        return observationRepository.save(observation);
+    public ResponseEntity<Observation> addObservation(@ModelAttribute Observation observation) {
+        Observation savedObservation = observationRepository.save(observation);
+        return new ResponseEntity<>(savedObservation, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public List<Observation> getObservations() {
-        return observationRepository.findAll();
+    public ResponseEntity<List<Observation>> getObservations() {
+        List<Observation> observations = observationRepository.findAll();
+        return new ResponseEntity<>(observations, HttpStatus.OK);
     }
 }
